dad32cb7e85176f728a6b7e6c7580082f255dfde
Katrina Acoba <61859816+katrinaacoba@users.noreply.github.com>
2020-03-06 17:03:18 +1100

    Update Users.sql

--- Users.sql
+++ Users.sql
@@ -1,1 +1,9 @@
+#To list all the users in the current database.
 
+SELECT usesysid AS user_id, 
+       usename  AS username, 
+       usesuper AS is_superuser, 
+       passwd   AS password_md5, 
+       valuntil AS password_expiration 
+FROM   pg_shadow 
+ORDER  BY usename; 
